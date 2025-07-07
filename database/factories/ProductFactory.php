7004ff31ed5e66464ca798b201b4ceb89b4a4cb4
jasonryans <i133040003@student.nsysu.edu.tw>
2025-07-07 12:45:38 +0700

    add factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => Category::factory(),
+            'name' => fake()->words(3, true),
+            'description' => fake()->paragraph(3),
+            'price' => fake()->randomFloat(2, 10, 1000),
+            'stock' => fake()->numberBetween(0, 100),
+        ];
+    }
+
+    /**
+     * Indicate that the product is out of stock.
+     */
+    public function outOfStock(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'stock' => 0,
+        ]);
+    }
+
+    /**
+     * Indicate that the product is in stock.
+     */
+    public function inStock(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'stock' => fake()->numberBetween(10, 100),
+        ]);
+    }
+}
